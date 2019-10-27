14b8001cc070cf12924323fcf1aee5824414e13d
Il'mira <ashilmira@gmail.com>
2019-10-27 23:12:46 +0300

    сделаны секции price и offer и начато feedback

--- js/main.js
+++ js/main.js
@@ -4,3 +4,20 @@ $('.menu-btn').on('click', function(event){
   $('.menu-nav').toggleClass('menu-nav_active');
 
 });
+
+$(document).ready(function(){
+  $('.slider-hero').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: false
+  });
+  $('.feedback-slider').slick({
+    infinite: true,
+    dots: true,
+    arrows: false,
+    slidesToShow: 2,
+    slidesToScroll: 1
+  });
+});
